220512b5c1cb51526558a3cc5b9ab891e6eb3bb0
Marcus Carlsson <marcuscarlsson@marcuss-macbook.home>
2019-02-10 11:28:41 +0000

    [ Exercism - Acronym ] Improved.
    Used string builder, also removed the regex because I didn't need it.

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -2,22 +2,21 @@
 package acronym
 
 import (
-	"regexp"
 	"strings"
+	"unicode"
 )
 
 // Abbreviate takes a string s and figures out what the acronym is.
 // "Ruby on Rails" -> "ROR"
 func Abbreviate(s string) string {
-	var result string
-	// Random regex, most be a better one out there.
-	fields := regexp.MustCompile("\\s|-|yperTex").Split(s, -1)
-	for _, field := range fields {
-		if len(field) == 0 {
-			continue
+	builder := strings.Builder{}
+	// Not sure about the replace here, it solves the issue, but it seems like a hack?
+	// The problem is that "Halley's Comet" become HSC.
+	normalized := strings.Replace(strings.ToLower(s), "'", "", -1)
+	for _, rune := range strings.Title(normalized) {
+		if unicode.IsLetter(rune) && unicode.IsUpper(rune) {
+			builder.WriteRune(unicode.ToUpper(rune))
 		}
-
-		result = result + strings.ToUpper(string(field[0]))
 	}
-	return result
+	return builder.String()
 }
