e6dca5a17dde03f93ce81f2d77d2bd6929a451f6
Marcus Carlsson <marcuscarlsson@marcuss-macbook.home>
2019-02-16 18:01:42 +0000

    [ Exercism - ETL ] Changed.
    Didn't understand the assignment, changed it to reflect it.

--- etl/etl.go
+++ etl/etl.go
@@ -1,41 +1,16 @@
 // Package etl extract transform load.
 package etl
 
-import (
-	"strings"
-	"unicode"
-)
+import "strings"
 
 // Transform transform old data to newer fancier data.
 func Transform(m map[int][]string) map[string]int {
 	result := make(map[string]int)
-	for _, s := range m {
-		for _, v := range s {
-			for _, s := range strings.ToLower(v) {
-				result[string(s)] += value(s)
-			}
+	for i, s := range m {
+		for _, value := range s {
+			key := strings.ToLower(value)
+			result[string(key)] += i
 		}
 	}
 	return result
-}
-
-func value(r rune) int {
-	switch unicode.ToUpper(r) {
-	case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
-		return 1
-	case 'D', 'G':
-		return 2
-	case 'B', 'C', 'M', 'P':
-		return 3
-	case 'F', 'H', 'V', 'W', 'Y':
-		return 4
-	case 'K':
-		return 5
-	case 'J', 'X':
-		return 8
-	case 'Q', 'Z':
-		return 10
-	default:
-		return 0
-	}
 }
