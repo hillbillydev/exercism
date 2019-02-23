6559514abb58fcf97d93ab5a4362267d000e0073
Marcus Carlsson <marcuscarlsson@marcuss-macbook.home>
2019-02-23 14:23:39 +0000

    [ Exercism - Isogram ] Better clear then clever.

--- isogram/cases.go
+++ isogram/cases.go
@@ -1,24 +1,28 @@
 // Package isogram helps you out figuring out if a string is a isogram.
 package isogram
 
-import "unicode"
+import (
+	"strings"
+	"unicode"
+)
 
 // IsIsogram determines if the passed in string is an Isogram.
 // it determines that by looking at each rune in the string
 // if the rune count goes over 1 we know it's not a Isogram.
 func IsIsogram(s string) bool {
-	runeMap := make(map[rune]struct{})
-	for _, r := range s {
-		r = unicode.ToLower(r)
+	runes := make(map[rune]bool)
+	for _, r := range strings.ToLower(s) {
 		if !unicode.IsLetter(r) {
 			continue
 		}
-		if _, exists := runeMap[r]; exists {
+		if v := runes[r]; v {
 			return false
 		}
 
-		runeMap[r] = struct{}{}
+		runes[r] = true
 	}
 
 	return true
 }
+
+// 200000	      6786 ns/op	    1095 B/op	      11 allocs/op
