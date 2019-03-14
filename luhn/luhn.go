667a125cc35d4aaca4baeafabe835ff0d0fd15f3
Tinee <Marcus.caarlsson92@gmail.com>
2019-03-14 22:06:49 +0000

    [ Exercism - Luhn ] Changed .

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -2,54 +2,40 @@
 package luhn
 
 import (
+	"regexp"
 	"strconv"
-	"unicode"
 )
+
+var trimSpaces = regexp.MustCompile(`[^ ]`).FindAllString
 
 // Valid determines the validity of the input, if it is an luhn sequence we give back true otherwise false.
 func Valid(input string) bool {
-	luhns, ok := toValidLuhns(input)
-	if !ok {
+	var (
+		sum     int
+		trimmed = trimSpaces(input, -1)
+		l       = len(trimmed)
+	)
+	if l <= 1 {
 		return false
 	}
 
-	var sum int
-	for i := len(luhns); i > 0; i-- {
-		current := luhns[i-1]
-		if (len(luhns)%2 == 0) == (i%2 == 0) {
-			sum += current
+	for i := l; i > 0; i-- {
+		n, err := strconv.Atoi(trimmed[i-1])
+		if err != nil {
+			return false
+		}
+
+		if (l%2 == 0) == (i%2 == 0) {
+			sum += n
 			continue
 		}
 
-		current *= 2
-		if current > 9 {
-			current -= 9
+		n *= 2
+		if n > 9 {
+			n -= 9
 		}
-
-		sum += current
+		sum += n
 	}
 
 	return sum%10 == 0
-}
-
-func toValidLuhns(s string) ([]int, bool) {
-	var luhns []int
-	for _, r := range s {
-		if unicode.IsSpace(r) {
-			continue
-		}
-
-		nr, err := strconv.Atoi(string(r))
-		if err != nil {
-			return nil, false
-		}
-
-		luhns = append(luhns, nr)
-	}
-
-	if len(luhns) <= 1 {
-		return nil, false
-	}
-
-	return luhns, true
 }
