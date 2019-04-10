5d7bf4e949367c9ecca4c947ed0c82fce0c41614
Tinee <Marcus.caarlsson92@gmail.com>
2019-04-10 12:49:15 +0200

    [ Exercism - Grains ] Bitwise math.

--- grains/grains.go
+++ grains/grains.go
@@ -3,14 +3,16 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 // Total gives the all the grains on each square on a chess board.
 func Total() uint64 {
 	var sum uint64
 	for i := 1; i <= 64; i++ {
-		v, _ := Square(i)
+		v, err := Square(i)
+		if err != nil {
+			continue
+		}
 		sum += v
 	}
 	return sum
@@ -21,6 +23,7 @@ func Square(i int) (uint64, error) {
 	if i < 1 || i > 64 {
 		return 0, errors.New("invalid argument")
 	}
-	grains := math.Pow(2, float64(i-1))
+	// grains := math.Pow(2, float64(i-1))
+	grains := 1 << uint(i-1)
 	return uint64(grains), nil
 }
