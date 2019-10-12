2ecd7f6cd71970cdeb8641b34ac56d95473ff44f
Marcus Karlsson <marcus.caarlsson92@gmail.com>
2019-10-12 19:15:09 +1300

    Tree made more readable.
    Still WIP

--- tree-building/tree.go
+++ tree-building/tree.go
@@ -24,42 +24,42 @@ func Build(records []Record) (*Node, error) {
 	}
 
 	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
+	var (
+		nodes    []*Node
+		lastRecord = records[len(records)- 1]
+		unique = make(map[int]bool)
+	)
+
+	// if the last records has an ID of 5 and the lenght is 4 in the slice
+	// it means that the slice is not continuous.
+	if lastRecord.ID > len(records) - 1 {
+		return nil, errors.New("Error: non-continuous nodes")
+	}
 
 	root, records := records[0], records[1:]
 	if root.ID != root.Parent {
-		return nil, fmt.Errorf("Error: Root node doesn't have the same ID(%d) as ParentID(%d)", root.ID, root.Parent)
+		return nil, fmt.Errorf("Error: Root node has a parent ID of %d", root.Parent)
 	}
 
-	var (
-		nodes    []*Node
-		rootNode = &Node{ID: root.ID}
-	)
-
+	rootNode := &Node{ID: root.ID}
 	nodes = append(nodes, rootNode)
 
-	var temp *Node
 	for _, r := range records {
 		if r.ID <= r.Parent {
 			return nil, fmt.Errorf("Error: didn't expect ID of %d to be less of equal to the parent of (%d)", r.ID, r.Parent)
 		}
 
-		temp = &Node{ID: r.ID}
+		if _, exists := unique[r.ID]; exists {
+			return nil, fmt.Errorf("Error: ID of %d is not unique.", r.ID)
+		}
+		unique[r.ID] = true
+
+		temp := &Node{ID: r.ID}
 		nodes = append(nodes, temp)
 		parent := nodes[r.Parent]
 		parent.Children = append(parent.Children, temp)
 	}
 
-	m := make(map[int]bool)
-	for _, n := range nodes {
-		if _, exists := m[n.ID]; exists {
-			return nil, fmt.Errorf("Error: ID of %d is not unique.", n.ID)
-		}
-		m[n.ID] = true
-	}
-
-	if nodes[len(nodes)- 1].ID > len(nodes) - 1 {
-		return nil, errors.New("Error: non-continuous nodes")
-	}
-
 	return rootNode, nil
 }
+
