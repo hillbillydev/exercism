2f90c9ebaee6baa3ab6889679469213852cbfcb9
Marcus Karlsson <marcus.caarlsson92@gmail.com>
2019-10-19 14:03:30 +1300

    Robot now handle collisions.
    It also doesn't have a predictable sequence.

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -2,9 +2,12 @@
 package robotname
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 )
+
+var generator = newNameGenerator()
 
 // Robot represents the core struct in this package.
 type Robot struct {
@@ -17,25 +20,70 @@ func (r *Robot) Name() (string, error) {
 	if r.name != "" {
 		return r.name, nil
 	}
-	r.name = generateRandomName()
+	name, err := generator.next()
+	r.name = name
 
-	return r.name, nil
+	return name, err
 }
 
-// Reset removes the old name and generate a new one.
+// Reset resets the name of the robot.
+// Don't worry by calling for a name you'll get a new back.
 func (r *Robot) Reset() {
-	r.name = generateRandomName()
+	r.name = ""
 }
 
-func generateRandomName() string {
+type nameGenerator struct {
+	names  []string
+	cursor int
+}
+
+func (g *nameGenerator) next() (string, error) {
+	if g.cursor == len(g.names) {
+		return "", errors.New("error: names exhausted")
+	}
+
+	next := g.names[g.cursor]
+	g.cursor++
+
+	return next, nil
+}
+
+func newNameGenerator() *nameGenerator {
+	return &nameGenerator{
+		names: generateNames(),
+	}
+}
+
+func generateNames() []string {
 	var (
-		numbers = 100 + rand.Intn(999-100)
-		letters = []rune{randomRune(), randomRune()}
+		result []string
+
+		first  = 'A'
+		second = 'A'
+		number = 0
 	)
 
-	return fmt.Sprintf("%s%d", string(letters), numbers)
-}
+	for first <= 'Z' {
+		result = append(result, fmt.Sprintf("%s%s%03d", string(first), string(second), number))
 
-func randomRune() rune {
-	return 65 + rune(rand.Intn(91-65))
+		if number != 999 {
+			number++
+			continue
+		}
+		number = 0
+		second++
+
+		if second <= 'Z' {
+			continue
+		}
+
+		first++
+		second = 'A'
+	}
+
+	rand.Shuffle(len(result), func(i, j int) {
+		result[i], result[j] = result[j], result[i]
+	})
+
+	return result
 }
