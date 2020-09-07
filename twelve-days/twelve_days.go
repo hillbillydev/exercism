4ec5228c5b06238340fc9540ccecc878a3009ca3
Marcus Karlsson <marcus.caarlsson92@gmail.com>
2020-09-07 14:51:18 +1200

    Exercism Twelve Days

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -2,57 +2,30 @@
 package twelve
 
 import (
-	"fmt"
 	"strings"
 )
 
-type lyric struct {
-	day  string
-	text string
-}
-
-var lyrics = []lyric{
-	lyric{"first", "a Partridge in a Pear Tree."},
-	lyric{"second", "two Turtle Doves"},
-	lyric{"third", "three French Hens"},
-	lyric{"fourth", "four Calling Birds"},
-	lyric{"fifth", "five Gold Rings"},
-	lyric{"sixth", "six Geese-a-Laying"},
-	lyric{"seventh", "seven Swans-a-Swimming"},
-	lyric{"eighth", "eight Maids-a-Milking"},
-	lyric{"ninth", "nine Ladies Dancing"},
-	lyric{"tenth", "ten Lords-a-Leaping"},
-	lyric{"eleventh", "eleven Pipers Piping"},
-	lyric{"twelfth", "twelve Drummers Drumming"},
+var lyrics = []string{
+	"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
+	"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	"On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
 }
 
 // Song sings a christmas song.
 func Song() string {
-	var result strings.Builder
-
-    for i, item := range lyrics {
-		result.WriteString(fmt.Sprintf("On the %s day of Christmas my true love gave to me: %s", item.day, item.text))
-
-		for j := i - 1; j >= 0; j-- {
-			prev := lyrics[j]
-			result.WriteRune(',')
-
-			if j == 0 {
-				result.WriteString(" and")
-			}
-			result.WriteString(fmt.Sprintf(" %s", prev.text))
-		}
-		result.WriteRune('\n')
-	}
-
-	return strings.TrimRight(result.String(), "\n")
+	return strings.Join(lyrics, "\n")
 }
 
-// Verse is stupid and don't like christmas songs, so he asks for whole song
-// then when the song is done he just writes down the verse he liked.
+// Verse is lazy and just listen to one verse.
 func Verse(skip int) string {
-	if skip > len(lyrics) {
-		return ""
-	}
-	return strings.Split(Song(), "\n")[skip-1]
+	return lyrics[skip-1]
 }
